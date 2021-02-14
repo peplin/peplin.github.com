9499d648d74beea70430de007fd3dbb8dec7358b
Christopher Peplin <chris.peplin@rhubarbtech.com>
2021-02-14 15:02:33 -0500

    Fix jPlayer styles

--- js/app.js
+++ js/app.js
@@ -1,38 +1,6 @@
 $(document).ready(function() {
-    loadPhotoGrid();
     loadAudioPlayers();
 });
-
-function loadPhotoGrid() {
-    if($("#photo-grid").length > 0) {
-        $.ajax({url: "https://api.flickr.com/services/rest",
-            data: {
-                method: "flickr.people.getPublicPhotos",
-                user_id: "52818162@N00",
-                per_page: 15,
-                format: "json",
-                api_key: "55115d73f4005b232247ecef716803f6"
-            },
-            dataType: "jsonp",
-            jsonp: "jsoncallback",
-            success: function(data) {
-                var grid = $("#photo-grid");
-                $(data.photos.photo).each(function(i, photo) {
-                    var element = "";
-                    photo.thumbnail = "https://farm" + photo.farm +
-                        ".static.flickr.com/" + photo.server + "/" +
-                        photo.id + "_" + photo.secret + "_" + "s.jpg";
-                    photo.url = "https://www.flickr.com/photos/" +
-                        photo.owner + "/" + photo.id;
-
-                    grid.append('<a target="_blank" href="' + photo.url + '">' +
-                            '<img alt="'+ photo.title + '"src="' + photo.thumbnail +
-                            '"/></a>');
-                });
-            }
-        });
-    }
-}
 
 function loadAudioPlayers() {
     $("ul.audio-playlist").each(function(i, playlist) {
